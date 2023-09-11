9a9be247ac3ad112daf0c3ea16e1c695995276d1
VikitoXD <rm551886@fiap.com.br>
2023-09-11 14:30:47 -0300

    alterando página mapa

--- ZeDoGuincho/src/main.jsx
+++ ZeDoGuincho/src/main.jsx
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: '/', 
+        element: <Login/>
+      },
+      {
+        path: '/mapa', 
+        element: <Mapa/>
+      },
+      {
+        path: '/Cadastro', 
+        element: <Cadastro/>
+      },
+      {
+        path: '/excluir/produtos/:id', 
+        element: <ExcluirProduto/>
+      },
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
