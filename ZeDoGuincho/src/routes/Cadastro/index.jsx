65a47a9ba61b3dabdd8645eaa9ad8fec657491ac
VikitoXD <rm551886@fiap.com.br>
2023-09-11 14:33:27 -0300

    Finalizando Página mapa

--- ZeDoGuincho/src/routes/Cadastro/index.jsx
+++ ZeDoGuincho/src/routes/Cadastro/index.jsx
@@ -14,6 +14,73 @@ export default function Mapa(){
           </div>
           <img className="user" alt="User" src="user.svg" />
         </header>
+        <div className="overlap">
+          <div className="group">
+            <div className="upload-image">
+              <div className="overlap-group">
+                <p className="selecione-imagens-do">Selecione imagens&nbsp;&nbsp;do veículo e local do resgate</p>
+                <div className="text-wrapper-2">Clique aqui</div>
+                <img className="upload" alt="Upload" src="upload.svg" />
+              </div>
+            </div>
+            <div className="overlap-wrapper">
+              <div className="div-wrapper">
+                <div className="text-wrapper-3">Solicitar</div>
+              </div>
+            </div>
+            <div className="text-wrapper-4">Solicitar resgate do veículo</div>
+            <div className="overlap-group-wrapper">
+              <div className="overlap-2">
+                <div className="form">
+                  <div className="rectangle-2" />
+                  <div className="rectangle-3" />
+                  <div className="rectangle-4" />
+                  <div className="rectangle-5" />
+                  <div className="rectangle-6" />
+                  <div className="rectangle-7" />
+                  <div className="rectangle-8" />
+                  <div className="rectangle-9" />
+                  <div className="rectangle-10" />
+                </div>
+                <div className="text-wrapper-5">*Modelo do veículo</div>
+                <div className="text-wrapper-6">*Peso da carga</div>
+                <div className="text-wrapper-7">*Peso da carga</div>
+                <div className="text-wrapper-8">*Comprimento</div>
+                <div className="text-wrapper-9">*Altura</div>
+                <div className="text-wrapper-10">observações</div>
+                <div className="text-wrapper-11">*Situação do resgate</div>
+                <div className="text-wrapper-12">*Número da apolice</div>
+                <div className="CEP"> *CEP</div>
+              </div>
+            </div>
+          </div>
+          <div className="overlap-group-wrapper">
+            <div className="overlap-2">
+              <div className="form">
+                <div className="rectangle-2" />
+                <div className="rectangle-3" />
+                <div className="rectangle-4" />
+                <div className="rectangle-5" />
+                <div className="rectangle-6" />
+                <div className="rectangle-7" />
+                <div className="rectangle-8" />
+                <div className="rectangle-9" />
+                <div className="rectangle-10" />
+              </div>
+              <div className="text-wrapper-5">*Modelo do veículo</div>
+              <div className="text-wrapper-6">*Peso da carga</div>
+              <div className="text-wrapper-7">*Peso da carga</div>
+              <div className="text-wrapper-8">*Comprimento</div>
+              <div className="text-wrapper-9">*Altura</div>
+              <div className="text-wrapper-10">observações</div>
+              <div className="text-wrapper-11">*Situação do resgate</div>
+              <div className="text-wrapper-12">*Número da apolice</div>
+              <div className="CEP"> *CEP</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         
   );
 };
