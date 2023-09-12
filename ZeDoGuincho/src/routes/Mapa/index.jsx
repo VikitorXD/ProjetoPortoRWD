fcaa9386d53f445c3aec0e147d3edbc34635692a
VikitoXD <rm551886@fiap.com.br>
2023-09-11 23:09:43 -0300

    Arrumando Página Solicitacao e mapa

--- ZeDoGuincho/src/routes/Mapa/index.jsx
+++ ZeDoGuincho/src/routes/Mapa/index.jsx
@@ -1,8 +1,37 @@
 export default function Mapa(){
 
     return(
-        <main>
-            <h1>Mapa</h1>
-        </main>
-    )
-}
+        <div className="solicitation-maps">
+      <div className="div">
+        <div className="overlap">
+          <header className="header">
+            <div className="logotipo">
+              <img className="logo-holding" alt="Logo holding" src="" />
+              <div className="rectangle" />
+              <div className="text-wrapper">Help</div>
+            </div>
+            <img className="user" alt="User" src="user.svg" />
+          </header>
+          <div className="logotipo">
+            <img className="logo-holding" alt="Logo holding" src="image.png" />
+            <div className="rectangle" />
+            <div className="text-wrapper">Help</div>
+          </div>
+        </div>
+        <div className="main">
+          <div className="search-bar">
+            <div className="overlap-group">
+              <div className="text-wrapper-2">Selecione sua localização...</div>
+              <img className="search" alt="Search" src="search.svg" />
+            </div>
+          </div>
+        </div>
+        <button className="button">
+          <div className="div-wrapper">
+            <div className="text-wrapper-3">Seguinte</div>
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
---
+++ ZeDoGuincho/src/routes/solicitacao/index.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+export default function Solicitacao(){
+
+    return (
+        <div className="situation-form">
+          <div className="div">
+            <header className="header">
+              <div className="logotipo">
+                <img className="logo-holding" alt="Logo holding" src="" />
+                <div className="rectangle" />
+                <div className="text-wrapper">Help</div>
+              </div>
+              <img className="user" alt="User" src="user.svg" />
+            </header>
+            <div className="group">
+              <div className="upload-image">
+                <div className="overlap-group">
+                  <p className="selecione-imagens-do">Selecione imagens&nbsp;&nbsp;do veículo e local do resgate</p>
+                  <div className="text-wrapper-2">Clique aqui</div>
+                  <img className="upload" alt="Upload" src="upload.svg" />
+                </div>
+              </div>
+              <div className="overlap-wrapper">
+                <div className="overlap">
+                  <div className="text-wrapper-3">Solicitar</div>
+                </div>
+              </div>
+              <div className="text-wrapper-4">Solicitar resgate do veículo</div>
+              <div className="overlap-group-wrapper">
+                <div className="overlap-2">
+                  <div className="form">
+                    <div className="rectangle-2" />
+                    <div className="rectangle-3" />
+                    <div className="rectangle-4" />
+                    <div className="rectangle-5" />
+                    <div className="rectangle-6" />
+                    <div className="rectangle-7" />
+                    <div className="rectangle-8" />
+                    <div className="rectangle-9" />
+                    <div className="rectangle-10" />
+                  </div>
+                  <div className="text-wrapper-5">*Modelo do veículo</div>
+                  <div className="text-wrapper-6">*Peso da carga</div>
+                  <div className="text-wrapper-7">*Peso da carga</div>
+                  <div className="text-wrapper-8">*Comprimento</div>
+                  <div className="text-wrapper-9">*Altura</div>
+                  <div className="text-wrapper-10">observações</div>
+                  <div className="text-wrapper-11">*Situação do resgate</div>
+                  <div className="text-wrapper-12">*Número da apolice</div>
+                  <div className="CEP"> *CEP</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    };
